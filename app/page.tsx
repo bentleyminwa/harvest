e656916e5b4927dcdf71d6ef08513314949543d9
bentleyminwa <bentleyminwa@gmail.com>
2025-04-23 14:36:38 +0300

    Add reusable Header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,21 @@
 import { PrimaryButton, SecondaryButton } from "@/components/ui/button";
+import Header from "@/components/ui/header";
 import { ourFarmPath, productsPath } from "@/utils/paths";
-import Image from "next/image";
 
 export default function Homepage() {
   return (
     <section className="">
-      <header className="relative w-full h-screen">
-        {/* hero image */}
-        <Image
-          src={"/images/ui/hero.jpg"}
-          alt="hero"
-          fill
-          priority
-          className="object-cover -z-10"
-        />
-        {/* overlay */}
-        <div className="absolute inset-0 bg-gradient-to-r from-black/30 to-black/30 via-black/30 -z-10"></div>
-        {/* content */}
-        <div className="text-white w-3/4 mx-auto pt-52">
-          <div className="w-2/3 space-y-7">
-            <h1 className="text-6xl font-bold">
-              Only organic and free range products
-            </h1>
-            <p className="w-2/3 font-semibold text-lg">
-              HARVEST Farm is family-owned and operated, growing high quality
-              hops, produce, and pasture-raised meats
-            </p>
-            <div className="flex gap-10 items-center">
-              <PrimaryButton href={productsPath}>Products</PrimaryButton>
-              <SecondaryButton href={ourFarmPath}>Our Farm</SecondaryButton>
-            </div>
-          </div>
+      <Header
+        image={{ src: "/images/ui/hero.jpg", alt: "Three cows hero image" }}
+        title="Only Organic and Free Range Products"
+        description="HARVEST Farm is family-owned and operated, growing high quality hops, produce, and pasture-raised meats"
+        className="h-screen"
+      >
+        <div className="flex gap-7">
+          <PrimaryButton href={productsPath}>Products</PrimaryButton>
+          <SecondaryButton href={ourFarmPath}>Our Farm</SecondaryButton>
         </div>
-      </header>
+      </Header>
     </section>
   );
 }
---
+++ components/ui/header.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+
+type ImageProps = {
+  src: string;
+  alt: string;
+};
+
+type HeaderProps = {
+  children: React.ReactNode;
+  image: ImageProps;
+  title: string;
+  pageHeader?: React.ReactNode;
+  description?: string;
+  className?: string;
+};
+
+export default function Header({
+  image,
+  title,
+  pageHeader,
+  description,
+  children,
+  className,
+}: HeaderProps) {
+  return (
+    <header className={`relative w-full ${className}`}>
+      {/* hero image */}
+      <Image
+        src={image.src}
+        alt={image.alt}
+        fill
+        priority
+        className="object-cover -z-10"
+      />
+      {/* overlay */}
+      <div className="absolute inset-0 bg-gradient-to-r from-black/30 to-black/30 via-black/30 -z-10"></div>
+      {/* content */}
+      <div className="text-white w-3/4 mx-auto pt-52">
+        <div className="w-2/3 space-y-7">
+          <h2>{pageHeader}</h2>
+          <h1 className="text-6xl font-bold">{title}</h1>
+          <p className="w-2/3 font-semibold text-lg">{description}</p>
+          <div>{children}</div>
+        </div>
+      </div>
+    </header>
+  );
+}
